7648e404e05d1f3c8d191d6c672f2b707471b9bf
Laura K. Kamfonik <lkamfonik@wireless1x-168-122-11-246.bu.edu>
2015-05-01 21:44:52 -0400

    establishing file directory for final submission  Please enter the commit message for your changes. Lines starting

--- FINAL PROJECT (WORK ON THIS)/fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -lib "secureip" -o "X:/551_Project/Kaboom_with_title_and_hbnb_4_24_2015/t_lfsr_isim_beh.exe" -prj "X:/551_Project/Kaboom_with_title_and_hbnb_4_24_2015/t_lfsr_beh.prj" "work.t_lfsr" "work.glbl" 
